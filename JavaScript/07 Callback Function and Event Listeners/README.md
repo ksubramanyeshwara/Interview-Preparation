607e15a592184dddc531c060d389c984b88f5db4
ksubramanyeshwara <subramanyeshwarak@gmail.com>
2025-05-03 22:51:40 +0530

    feat: add callback and event listeners for call back

---
+++ JavaScript/07 Callback Function and Event Listeners/README.md
@@ -0,0 +1,14 @@
+# Callback Function and Event Listeners
+
+## Callback Function
+
+Callback function is a function that is passed as an argument to another function and executed after the parent function completes its task.
+
+null vs undefined:
+
+- `null` represents an intentional absence of any object value.
+- `undefined` represents an intentional absence of any value.
+
+feedback from interview team -> null
+hr -> job hai  -> null
+what is the interview result -> undefined
---
+++ JavaScript/07 Callback Function and Event Listeners/index.js
@@ -0,0 +1,30 @@
+// Synchronous Callback Function
+function greet(name, format) {
+  console.log("Welcome!");
+  console.log(1 + 1);
+
+  return `Hello ${format(name)}`;
+}
+
+function format(name) {
+  return name.toUpperCase();
+}
+
+console.log(greet("K Subramanyeshwara", format));
+
+// Asunchronous Callback Function
+console.log("Start");
+setTimeout(() => {
+  console.log("Asynchronous Callback Function");
+}, 5000);
+console.log("End");
+
+Event Listeners
+function clickMe() {
+  let countClick = 0;
+  const button = document.getElementById("button");
+  button.addEventListener("click", () => {
+    console.log(`Button Clicked ${++countClick} times`);
+  });
+}
+clickMe();
